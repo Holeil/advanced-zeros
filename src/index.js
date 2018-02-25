690799123de468b63697d0c0812c54f849c36714
Holeil <holeilaccougm@gmail.com>
2018-02-25 13:23:18 +0300

    Commit 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
+module.exports = function getZerosCount(number, n) {
+  function prostoe(n) {
+    var j = 0, i = 2, a = [];
+    do {
+      if(n%i == 0) {
+        a[j] = i;
+        j++;
+        n/=i;
+      }
+      else {
+        i++;
+      }
+    }while(i < n);
+    a[j]=i;
+    var array = [];
+    array[0] = a[0];
+    array[1] = 1;
+    for(var i=1; i < a.length; i++) {
+      if(a[i] == array[0]) {
+        array[1] += 1;
+      }
+      else if(a[i] > array[0]) {
+        array[0] = a[i];
+        array[1] = 1;
+      }
+    }
+    return array;
+  }
+  var count=0, array = prostoe(n);
+  while(number > array[0]) {
+    number = Math.floor(number/array[0]);
+    count = count + number;
+  }
+  return Math.floor(count/array[1]);
 }
